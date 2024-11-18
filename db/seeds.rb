22e5abba29970013e5b1eb7d0617fead95437d9c
vando <vandorramos@gmail.com>
2024-11-17 23:48:14 -0300

    Turbo Streams and security

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,13 @@
-puts "\n== Seeding the database with fixtures =="
-system("bin/rails db:fixtures:load")
+# puts "\n== Seeding the database with fixtures =="
+# system("bin/rails db:fixtures:load")
+
+kmpg = Company.create!(name: 'kmpg')
+pwc = Company.create!(name: 'pwc')
+
+User.create!(email: 'accountant@kpmg.com', password: 'password', company: kmpg)
+User.create!(email: 'manager@kpmg.com', password: 'password', company: kmpg)
+User.create!(email: 'eavesdropper@pwc.com', password: 'password', company: pwc)
+
+Quote.create!(name: 'First quote', company: kmpg)
+Quote.create!(name: 'Second quote', company: kmpg)
+Quote.create!(name: 'Third quote', company: kmpg)
