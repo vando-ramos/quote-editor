e3108bdaa31de9cbf2c19d6f6a296f66fe955b91
vando <vandorramos@gmail.com>
2024-12-09 23:21:03 -0300

    CRUD dates with turbo

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@ User.create!(email: 'accountant@kpmg.com', password: 'password', company: kmpg)
 User.create!(email: 'manager@kpmg.com', password: 'password', company: kmpg)
 User.create!(email: 'eavesdropper@pwc.com', password: 'password', company: pwc)
 
-Quote.create!(name: 'First quote', company: kmpg)
-Quote.create!(name: 'Second quote', company: kmpg)
-Quote.create!(name: 'Third quote', company: kmpg)
+first = Quote.create!(name: 'First quote', company: kmpg)
+second = Quote.create!(name: 'Second quote', company: kmpg)
+third = Quote.create!(name: 'Third quote', company: kmpg)
+
+LineItemDate.create!(quote: first, date: Date.current)
+LineItemDate.create!(quote: first, date: Date.current + 1.week)
