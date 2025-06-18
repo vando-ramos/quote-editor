be31af2a5174f8ad99fcf76e143cab16521a0b70
vando ramos <vandorramos@gmail.com>
2025-06-18 09:29:02 -0300

    Add a sticky bar to the Quotes#show page containing the total amount of the quot. Create a style component for total price

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
+  describe "#total_price" do
+    it "returns the total price of the line item" do
+      kmpg = Company.create!(name: 'KMPG')
+      user = User.create!(email: 'accountant@kpmg.com', password: 'password', company: kmpg)
+      quote = Quote.create!(name: "First quote", company: kmpg)
+      lid = LineItemDate.create!(date: Date.current, quote: quote)
+      line_item = LineItem.create!(name: "Room today", quantity: 3, unit_price: 500, line_item_date: lid)
+
+      expect(line_item.total_price).to eq(1500)
+    end
+  end
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Quote, type: :model do
+RSpec.describe Quote, type: :model do  
+  describe "#total_price" do
+    it "returns the sum of the total price of all line items" do
+      kmpg = Company.create!(name: 'KMPG')
+      user = User.create!(email: 'accountant@kpmg.com', password: 'password', company: kmpg)
+      quote = Quote.create!(name: "First quote", company: kmpg)
+      lid = LineItemDate.create!(date: Date.current, quote: quote)
+      line_item = LineItem.create!(name: "Room today", quantity: 3, unit_price: 500, line_item_date: lid)
+      line_item = LineItem.create!(name: "Catering", quantity: 10, unit_price: 100, line_item_date: lid)
+
+      expect(quote.total_price).to eq(2500)
+    end
+  end
 end
