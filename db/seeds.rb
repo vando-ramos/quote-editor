3e0bd14ce04d968e82a61cb07175c08347ee1f57
vando ramos <vandorramos@gmail.com>
2025-06-17 23:22:49 -0300

    Create mvc and style component for line item

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,14 @@ first = Quote.create!(name: "First quote", company: kmpg)
 second = Quote.create!(name: "Second quote", company: kmpg)
 third = Quote.create!(name: "Third quote", company: kmpg)
 
-LineItemDate.create!(date: Date.current, quote: first)
-LineItemDate.create!(date: Date.current + 1.week, quote: first)
+today = LineItemDate.create!(date: Date.current, quote: first)
+next_week = LineItemDate.create!(date: Date.current + 1.week, quote: first)
+
+LineItem.create!(name: 'Meeting room', description: 'A cosy meeting room for 10 people', quantity: 1, 
+                 unit_price: 1000, line_item_date: today)
+LineItem.create!(name: 'Meal tray', description: 'Our delicious meal tray', quantity: 10, 
+                 unit_price: 25, line_item_date: today)
+LineItem.create!(name: 'Meeting room', description: 'A cosy meeting room for 10 people', quantity: 1, 
+                 unit_price: 1000, line_item_date: next_week)
+LineItem.create!(name: 'Meal tray', description: 'Our delicious meal tray', quantity: 10, 
+                 unit_price: 25, line_item_date: next_week)
